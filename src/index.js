064242e9db2176b95f3378f19e82166445cb6bab
Ruchir Saxena <ruchirsaxena.coder@gmail.com>
2021-07-18 22:35:58 +0530

    Explanation of Big O Notation with theory and some examples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Big O Notation</h1>
+<h1>Big O Notation</h1> 
+<h3> Find Notes.txt for the explaination of Big 0 </h3>
+<hr/>
+<pre>function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+</pre>
+addUpTo(16) Result : ${addUpTo(16)} <hr/>
+Big O Notation : O(n)
+
 `;
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
